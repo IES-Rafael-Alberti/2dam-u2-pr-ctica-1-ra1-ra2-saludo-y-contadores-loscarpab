a87553d7cc764dd09594de84a20ab8f7ad340f92
loscarpab <ccormor392@g.educaand.es>
2023-11-13 13:21:24 +0100

    commit de prueba

--- app/src/main/java/com/ccormor392/saludoscontadores/MainActivity.kt
+++ app/src/main/java/com/ccormor392/saludoscontadores/MainActivity.kt
@@ -3,11 +3,19 @@ package com.ccormor392.saludoscontadores
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.ccormor392.saludoscontadores.ui.theme.SaludosContadoresTheme
@@ -22,25 +30,23 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainBoton()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SaludosContadoresTheme {
-        Greeting("Android")
+fun MainBoton() {
+    var texto by rememberSaveable{ mutableStateOf("")}
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Saludar")
+        }
+        Text(text = texto)
     }
 }
